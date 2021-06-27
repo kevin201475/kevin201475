f8f8357a51c85b8b7d3968be5bebcbc4bf1926ca
Kevin Kuruvilla <kevin.kuruvilla@gmail.com>
2021-06-27 12:04:15 -0600

    fibonacci in python

---
+++ fibonacci.py
@@ -0,0 +1,9 @@
+def fibonacci(numOfIter):
+    fibSeriesList,nextEleInFib = [0],1
+    for _ in range(numOfIter):
+        fibSeriesList.append(nextEleInFib)
+        nextEleInFib = fibSeriesList[-1]+fibSeriesList[-2]
+    return fibSeriesList
+        
+
+print(fibonacci(5))
